95912e55651a8970c86243e62cb9447e8e28018e
Lakshan <lakshan.m@ritzclothing.lk>
2026-06-20 20:30:47 +0530

    minor changes

--- solarops-frontend/src/api/axios.ts
+++ solarops-frontend/src/api/axios.ts
@@ -1,5 +1,18 @@
+// import axios from 'axios'
+// export const api = axios.create({
+//     baseURL: 'http://localhost:4000/api',
+//     timeout: 10000,
+// })
+
+
 import axios from 'axios'
+
+// Checks if a live environment variable exists, otherwise falls back to localhost for your machine
+const BASE_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:4000/api';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: BASE_URL,
     timeout: 10000,
 })
