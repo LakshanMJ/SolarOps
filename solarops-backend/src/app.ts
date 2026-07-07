92bce237e88b94deb7b2936996de8e237565fd01
Lakshan <lakshan.m@ritzclothing.lk>
2026-07-07 11:20:30 +0530

    fixes

--- solarops-backend/src/app.ts
+++ solarops-backend/src/app.ts
@@ -30,7 +30,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/auth", authRoutes);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use("/api/upload-image", uploadRoutes);
 
 app.use('/api/dashboard', dashboardRoutes)
